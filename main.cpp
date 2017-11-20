518e7e944fff305434fb315513132810c0feedf3
bilal işler <bilal.isler@enuygun.com>
2017-11-20 21:04:13 +0300

    update files

--- main.cpp
+++ main.cpp
@@ -1,25 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: bilal
- *
- * Created on November 20, 2017, 8:40 PM
- */
-
 #include <cstdlib>
+#include <cstdio>
 
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
 
+    printf("%d",3);
+     
     return 0;
 }
 
