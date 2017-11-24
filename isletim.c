34e694e6c9349d42dc81f40c68d6af81d907b404
omerfarukgeris <omerfarukgeris@gmail.com>
2017-11-24 22:36:38 +0300

    t

--- isletim.c
+++ isletim.c
@@ -6,10 +6,12 @@
 #include "unistd.h"
 #include "string.h"
 int digit_command=5;
-char *commands[]={"ls","cat","echo","file"};
+char *commands[]={"","ls","cat","echo","file","\0"};
 
 char *operators[]={"<",">","&","|",";"};
 char *list[20];
+const char *quit="quit";
+const char *s=">";
 
 int status;
 int digit_input;
@@ -24,6 +26,7 @@ int digit_child=0;
 void cntrl(char []);
 void cntrl_param();
 int run_command(char *cmd,char *params[]);
+
 
 void cntrl( char input[]){	
 	
@@ -47,38 +50,52 @@ void cntrl( char input[]){
 
 void cntrl_param(){
 	char *cmd;
-	 cmd=malloc(sizeof(char *));
+	cmd=malloc(sizeof(char*));
 	char *param[20];
 	*param=malloc(sizeof(char*)*20);
 	char *opr[20];
 	int k=0,l=0,m=0;
+
 	for (int i = 0; i < digit_input; ++i)
 	{	
 		for (int j = 0; j < sizeof(commands)/(sizeof(char *)) ; ++j)
 		{	
-			if(strcmp(list[i],commands[j])==0){
-				
+			if(strcmp(list[i],quit)==0){
+				exit(0);;
+			}
+			else if (strcmp(list[i],commands[j])==0)
+			{
 				cmd=commands[j];
+			}else if(strcmp(operators[j],list[i])==0){
+				opr[m++]=operators[j];
+				printf("%s",operators[j]);
+				if(strcmp(operators[j],s)==0){
+					printf("as");
+					if(strlen(list[i+1])!=0){
 
-			}
-			else if(strcmp(list[i],operators[j])==0){
-				opr[l++]=operators[j];	
-			}else{
+					}else{
+						printf("Erorr Newline");
+					}
+				}
+
+				
+			}else if(strcmp(param[k-1],list[i])!=0 ){
 				param[k++]=list[i];
-				printf("---%s",param[k-1]);
-				break;
+					printf("aq");
 			}
+
 
 		}
 	}
 	
 	param[k]=NULL;
 	run_command(cmd,param);
-
+	
+	
 }
 
 int run_command(char *cmd, char *param[]){
-	 printf("cmd=%s.param1=%s.param2=%s\n",cmd,param[0],param[1]);
+	 //printf("cmd=%s.param1=%s.param2=%s\n",cmd,param[0],param[1]);
 	struct Child *child;
 	 
 	child=malloc(sizeof(struct Child*));
@@ -86,9 +103,9 @@ int run_command(char *cmd, char *param[]){
 	
 	if((child->pid=fork())<0){
 		printf("ERROR _child process failed\n");
-        exit(1);
+        exit(1);	
 	}else if(child->pid==0){
-		
+			
 		if(execvp(cmd,param)<0){
 
 			printf("ERROR _exec failed-%s-  %d\n",cmd,execvp(cmd,param));
